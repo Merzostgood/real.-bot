a609dca047067b0b9e84cf1c0869079ea6d36031
Tyher228 <86001086+Tyher228@users.noreply.github.com>
2024-03-19 12:01:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,4 +14,4 @@ async def spam(ctx, *, arg):
     while True:
         await arg.send("Тест")
 
-bot.run("MTE5ODk1ODA2MzIwNjUzOTI4NQ.G4DuUi.U9usc0-KG-p8R1roQ5GWR9ibWymqLl84DRWpu4")
+bot.run("Here token")
